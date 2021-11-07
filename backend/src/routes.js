6bfd5915cf0e316c9557e0687d1dd38424c35c1e
Aída Rocha <88014540+aidarocha@users.noreply.github.com>
2021-11-07 11:29:40 -0300

    Create routes.js

---
+++ backend/src/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const CandidateController = require('./controllers/CandidateController');
+const routes = new express.Router();
+routes.post('/register', CandidateController.register);
+
+module.exports = routes;
